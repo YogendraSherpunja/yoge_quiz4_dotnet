961c3b7547676606fd726d05e9d97aacad9194d4
YogendraSherpunja <yogendramagar15@gmail.com>
2025-04-10 13:57:32 -0400

    Added database access message to web app

--- yoge_quiz4_dotnet/obj/Debug/net8.0/yoge_quiz4_dotnet.AssemblyInfo.cs
+++ yoge_quiz4_dotnet/obj/Debug/net8.0/yoge_quiz4_dotnet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("yoge_quiz4_dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5e136b50f8fb625f38ac8ef2bd6fcfbadf5a854")]
 [assembly: System.Reflection.AssemblyProductAttribute("yoge_quiz4_dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("yoge_quiz4_dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
